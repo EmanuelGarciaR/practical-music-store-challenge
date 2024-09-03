5166fda46e115c9fac0ce88c1888da8b611df51f
Ema <emanuelgarciarios@gmail.com>
2024-09-02 22:22:16 -0500

    Create  Disc class

--- musicstore/model.py
+++ musicstore/model.py
@@ -1,10 +1,49 @@
 from datetime import datetime
 
+
 # TODO: Add code here
 class Transaction:
     SELL: int = 1
     SUPPLY: int = 2
+
     def __init__(self, type: int, copies: int):
         self.type: int = type
         self.copies: int = copies
         self.date: datetime = datetime.now()
+
+
+class Disc:
+    def __init__(self, sid: str, title: str, artist: str, sale_price: float, purchase_price: float, quantity: int):
+        self.sid: str = sid
+        self.title: str = title
+        self.artist: str = artist
+        self.sale_price: float = sale_price
+        self.purchase_price: float = purchase_price
+        self.quantity: int = quantity
+        self.transactions: list[Transaction] = []
+        self.song_list: list[str] = []
+
+    def add_song(self, song: str):
+        self.song_list.append(song)
+
+    def sell(self, copies: int) -> bool:
+        if copies > self.quantity:
+            return False
+        else:
+            self.quantity -= copies
+            self.transactions.append(Transaction(Transaction.SELL, copies))
+            return True
+
+    def supply(self, copies: int):
+        self.quantity += copies
+        self.transactions.append(Transaction(Transaction.SUPPLY, copies))
+
+    def copies_sold(self) -> int:
+        total_copies = 0
+        for transaction in self.transactions:
+            if transaction.type == Transaction.SELL:
+                total_copies += transaction.copies
+        return total_copies
+
+    def __str__(self)-> str:
+        return f"SID: {self.sid}\nTitle: {self.title}\nArtist: {self.artist}\nSong List: {", ".join(self.song_list)}"
